05f7497b367cc44bae1ece18de2ecbf52a5877c3
panades-git <oscarpanades@hotmail.com>
2020-09-26 10:32:00 +0200

    Header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from './assets/img/logo.svg';
+import Header from './components/header';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Fight React APP
-        </p>
-      </header>
+      <Header />
     </div>
   );
 }
